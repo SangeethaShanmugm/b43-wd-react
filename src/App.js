eeb9338de3cd80b8c6fec180e7c620155916e231
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-02-23 11:56:50 +0530

    added AddBook form

--- src/App.js
+++ src/App.js
@@ -115,12 +115,60 @@ function BookDetails() {
 }
 
 function BookList() {
-  const bookList = INITIAL_BOOK_LIST;
+  //const bookList = INITIAL_BOOK_LIST;
+  const [bookList, setBookList] = useState(INITIAL_BOOK_LIST);
+  const [name, setName] = useState("");
+  const [poster, setPoster] = useState("");
+  const [rating, setRating] = useState("");
+  const [summary, setSummary] = useState("");
+
   return (
-    <div className="book-list">
-      {bookList.map((bk, index) => (
-        <Book key={index} book={bk} id={index} />
-      ))}
+    <div>
+      <div className="add-book-form">
+        <input
+          type="text"
+          onChange={(event) => setName(event.target.value)}
+          placeholder="Enter the name"
+        />
+        <input
+          type="text"
+          onChange={(event) => setPoster(event.target.value)}
+          placeholder="Enter the poster"
+        />
+        <input
+          type="text"
+          onChange={(event) => setRating(event.target.value)}
+          placeholder="Enter the rating"
+        />
+        <input
+          type="text"
+          onChange={(event) => setSummary(event.target.value)}
+          placeholder="Enter the summary"
+        />
+
+        <button
+          // copy the bookList and add newBook to it
+
+          onClick={() => {
+            const newBook = {
+              name: name,
+              poster: poster,
+              rating: rating,
+              summary: summary,
+            };
+
+            setBookList([...bookList, newBook]);
+          }}
+        >
+          Add Book
+        </button>
+      </div>
+
+      <div className="book-list">
+        {bookList.map((bk, index) => (
+          <Book key={index} book={bk} id={index} />
+        ))}
+      </div>
     </div>
   );
 }
